19dc1c0a5b92becd628f004b90f3320d3a310d9a
gkatabi <91203613+gkatabi@users.noreply.github.com>
2024-09-24 06:55:29 +0200

    Update Program.cs
    git -m "magic number"

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,40 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+    {
+        Try to restructure the logic so it does exactly what you want.
+
+Random r = new Random();
+
+int val = r.Next(1, 100);
+int guess = 0;
+bool correct = false;
+
+Console.WriteLine("I'm thinking of a number between 1 and 100.");
+
+while (!correct)
+{
+    Console.Write("Guess: ");
+    string input = Console.ReadLine();
+
+    if (!int.TryParse(input, out guess))
+    {
+        Console.WriteLine("That's not a number.");
+        continue;
+    }
+
+    if (guess < val)
+    {
+        Console.WriteLine("No, the number I'm thinking is higher than that number.");
+    }
+    else if (guess > val)
+    {
+        Console.WriteLine("No, the number I'm thinking is lower than that number.");
+    }
+    else
+    {
+        correct = true;
+        Console.WriteLine("You guessed right!");
+    }
     }
 }
