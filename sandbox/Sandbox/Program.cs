9c7f00d467dc809f312e7c4039a2b0151f62faad
gkatabi <91203613+gkatabi@users.noreply.github.com>
2024-09-25 12:29:58 +0200

    Update Program.cs
    "improved it"

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,10 +4,17 @@ public class jobResume
 {
     public string_jobName = " "
     public string_jobResponsibility = " "
-    public int_startYear = ""
-    public int_endYear = ""
-    {
-        Console.WriteLine("Hello Sandbox World!");
+    public int_startYear = "";
+    public int_endYear = "";
+    
+        public job( )
+        {
+        }
+        public void ShowJobName()
+        {
+            Console.WriteLine($"{_jobName}";
+Console.WriteLine($"as a{"_jobResponsibility}";
 
-    }
+
+        }
 }
