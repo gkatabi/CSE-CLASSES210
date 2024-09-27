4369fd19551e9a851048b69c26aa49c84ab53bc7
gkatabi <91203613+gkatabi@users.noreply.github.com>
2024-09-27 11:08:00 +0200

    Update Program.cs
    "ok"

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,35 @@
 using System;
 
-class Program
+namespace HelloWorld
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Prep2 World!");
+        static void Main(string[] args)
+        {
+            /* >= 90...A
+              >= 80...B
+              >= 70...C
+              >= 60...D
+              <  50...F*/
+            Console.WriteLine("This is the grade finder");
+            Console.WriteLine("What was your score?: ");
+            string score = Console.ReadLine();
+            int Score = Convert.ToInt32(Console.ReadLine());
+            if (Score >= 90)
+            {
+                Console.WriteLine("You passed with: A");
+            else if (Score >= 80)
+                    Console.WriteLine("You have passed: B");
+                else if (Score >= 70)
+                    Console.WriteLine("You passed with:C");
+                if (Score < 60)
+                    Console.WriteLine("You passed with: D");
+            }
+            else
+            {
+                Console.WriteLine("You failed with: F");
+
+            }
+        }
     }
 }
