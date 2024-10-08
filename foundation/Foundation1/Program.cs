a76302f1fbbd0120d7c0ea4e242266dec34c1bad
gkatabi <91203613+gkatabi@users.noreply.github.com>
2024-10-08 03:45:56 +0200

    Update Program.cs
    tried adding method

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     Abstract class video
     {
+        public Abstract void video();
+        //start function here
+        public void.....();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Foundation1 World!");
